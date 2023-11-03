cc557d8651f0aaa8f36e6f0bbcde452dbb4e9e55
useless444 <habibhk127@gmail.com>
2023-11-03 21:09:21 +0600

    User DB

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+
 app.use(cors());
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
-
-
-
-
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -33,6 +32,32 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+
+    //Collection List
+    const usersDB = client.db("movieB2R").collection("userCollection");
+
+
+
+
+    // Operations
+
+    app.get('/userList', async (req, res) => {
+      const list = await usersDB.find().toArray();
+      res.send(list);
+    })
+    app.post('/addUser', async (req, res) => {
+      const data = req.body;
+      const userEmail = { email : data.email}
+      const isExisting = await usersDB.findOne(userEmail);
+      if(isExisting){
+          res.send({Exist: true})
+      }
+      else{
+        const addedUser = await usersDB.insertOne(data)
+        res.send({ addedUser,Exist : false});
+      }
+      
+    })
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
